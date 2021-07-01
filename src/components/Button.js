16251f5d5575ca135c488810222864d5648b0725
smcommits <sm.anas@outlook.com>
2021-07-01 19:02:08 +0530

    Fix test warnings

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,6 +2,11 @@ import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import * as style from './componentStyles/button.module.css';
 
+
+const StyleButton = styled.button`
+    background: ${(props) => (props.color === 'white' ? 'white' : 'orange')};
+    width: ${(props) => (props.wide ? '50%' : '25%')};
+  `;
 const Button = (props) => {
   const {
     clickHandler, color, wide, name,
@@ -10,11 +15,6 @@ const Button = (props) => {
   const handleClick = () => {
     clickHandler(name);
   };
-
-  const StyleButton = styled.button`
-    background: ${(props) => (props.color === 'white' ? 'white' : 'orange')};
-    width: ${(props) => (props.wide ? '50%' : '25%')};
-  `;
 
   return (
     <StyleButton className={style.calculatorButton} type="submit" onClick={handleClick} color={color} wide={wide}>
@@ -27,11 +27,12 @@ Button.propTypes = {
   name: PropTypes.string,
   clickHandler: PropTypes.func.isRequired,
   color: PropTypes.string.isRequired,
-  wide: PropTypes.bool.isRequired,
+  wide: PropTypes.bool,
 };
 
 Button.defaultProps = {
   name: 'Some Button',
+  wide: false
 };
 
 export { Button as default };
